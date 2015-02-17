0c21f7723ebab88551bc4409711ee68f48be997f
Leonardo Val <leonardo.val@creatartis.com>
2015-02-17 16:42:48 -0200

    Distribution estimation as Problem
    An `histogramProblem` is an distribution estimation defined as a problem
    that can be resolved by any metaheuristic.

--- src/metaheuristics/DistributionEstimation.js
+++ src/metaheuristics/DistributionEstimation.js
@@ -19,7 +19,7 @@ var DistributionEstimation = metaheuristics.DistributionEstimation = declare(Met
 			/** + `histogramWidth=10` is the amounts of ranges the value domain is split in order
 			to calculate the histograms.
 			*/
-			.integer('histogramWidth', { coerce: true, defaultValue: 10, minimum: 1 });
+			.integer('histogramWidth', { coerce: true, defaultValue: 10, minimum: 2 });
 	},
 	
 	/** New elements to add to the state in the `expansion` are build from the `histograms`
@@ -37,15 +37,17 @@ var DistributionEstimation = metaheuristics.DistributionEstimation = declare(Met
 	
 	/** The `histograms` have the frequencies of value ranges in the current state.
 	*/
-	histograms: function histograms(state) {
-		state || (state = this.state);
-		var histogramWidth = this.histogramWidth,
-			size = this.state.length,
+	histograms: function histograms() {
+		return DistributionEstimation.histograms(this.state, this.histogramWidth, 
+			this.problem.representation.prototype.length);
+	},
+	
+	'static histograms': function histograms(state, histogramWidth, histogramCount) {
+		var size = state.length,
 			emptyCount = base.Iterable.repeat(0, histogramWidth).toArray(),
-			length = this.problem.representation.prototype.length,
 			counts = base.Iterable.iterate(function (v) {
 				return v.slice(); // Shallow copy.
-			}, emptyCount, length).toArray();
+			}, emptyCount, histogramCount).toArray();
 		state.forEach(function (element) {
 			element.values.forEach(function (value, i) {
 				var bar = Math.min(histogramWidth - 1, Math.floor(element.values[i] * histogramWidth));
@@ -61,11 +63,13 @@ var DistributionEstimation = metaheuristics.DistributionEstimation = declare(Met
 	
 	/** The method `elementFromHistogram` is used to make these new random elements.
 	*/
-	elementFromHistograms: function elementFromHistogram(histograms, representation) {
-		representation || (representation = this.problem.representation);
+	elementFromHistograms: function elementFromHistogram(histograms) {
+		return DistributionEstimation.elementFromHistograms(histograms, this.problem.representation, this.random);
+	},
+	
+	'static elementFromHistograms': function elementFromHistogram(histograms, representation, random) {
 		var length = histograms.length,
 			values = new Array(length),
-			random = this.random,
 			histogram, r;
 		for (var i = 0; i < length; ++i) {
 			histogram = histograms[i];
@@ -79,6 +83,64 @@ var DistributionEstimation = metaheuristics.DistributionEstimation = declare(Met
 		}
 		return new representation(values);
 	},
+	
+	// ## Estimation of distribution as a problem. #################################################
+	
+	/** A `histogramProblem` is the problem of finding histograms that would generate good candidate
+	solutions for a given `problem`.
+	*/
+	'static histogramProblem': function histogramProblem(problem, size, histogramWidth) {
+		size = isNaN(size) ? 30 : Math.max(1, size |0);
+		histogramWidth = isNaN(histogramWidth) ? 10 : Math.max(2, histogramWidth |0);
+		var elementLength = problem.representation.prototype.length,
+			elementFromHistograms = this.elementFromHistograms;
+		return new Problem({
+			/** Each element of this problem represents an histogram for elements of the given
+			`problem`. The argument `histogramWidth` defines how many ranges each histogram has.
+			*/
+			representation: declare(Element, {
+				length: elementLength * histogramWidth,
+				
+				random: problem.random,
+				
+				/** The evaluation of the elements is the average evaluation of `size` elements 
+				generated from the histogram that this element represents.
+				*/
+				evaluate: function evaluate() {
+					var element = this,
+						histograms = this.mapping(),
+						elements = base.Iterable.repeat(null, size).map(function () {
+							return elementFromHistograms(histograms, problem.representation, problem.random);
+						});
+					return Future.all(iterable(elements).map(function (e) {
+						return Future.when(e.evaluate());
+					})).then(function (evaluations) {
+						return element.evaluation = iterable(evaluations).sum() / evaluations.length;
+					});
+				},
+				
+				/** The `mapping` simply assembles the histograms and normalizes its frequencies.
+				*/
+				mapping: function mapping() {
+					var histograms = [], histogram, sum;
+					for (var i = 0; i < elementLength; ++i) {
+						histogram = this.values.slice(i * histogramWidth, (i+1) * histogramWidth);
+						sum = iterable(histogram).sum();
+						histograms[i] = histogram.map(function (f) { // Normalization
+							return f / sum;
+						});
+					}
+					return histograms;
+				}
+			}),
+			
+			/** The comparison function is the same as the original problem's.
+			*/
+			compare: problem.compare
+		});
+	},
+	
+	// ## Other ####################################################################################
 	
 	toString: function toString() {
 		return (this.constructor.name || 'DistributionEstimation') +'('+ JSON.stringify(this) +')';
