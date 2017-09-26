9e7e8d7dd7d44be32972e97616a291c07e167f5d
Leonardo Val <leonardo.val@creatartis.com>
2017-09-26 12:01:09 -0300

    Updated `creatartis-grunt`

--- tests/require-config.js
+++ tests/require-config.js
@@ -1,13 +1,27 @@
 // Generated code, please do NOT modify.
 (function () { "use strict";
-	var config = {
-		"paths": {
-			"inveniemus": "../build/inveniemus",
-			"creatartis-base": "../node_modules/creatartis-base/build/creatartis-base.min",
-			"sermat": "../node_modules/sermat/build/sermat-umd",
-			"dygraphs": "../node_modules/dygraphs/dist/dygraph.min"
+	define([], function () {
+		var config = {
+			"paths": {
+				"inveniemus": "../build/inveniemus",
+				"creatartis-base": "../node_modules/creatartis-base/build/creatartis-base.min",
+				"sermat": "../node_modules/sermat/build/sermat-umd",
+				"dygraphs": "../node_modules/dygraphs/dist/dygraph.min"
+			}
+		};
+		if (window.__karma__) {
+			config.baseUrl = '/base';
+			for (var p in config.paths) {
+				config.paths[p] = config.paths[p].replace(/^\.\.\//, '/base/');
+			}
+			config.deps = Object.keys(window.__karma__.files) // Dynamically load all test files
+				.filter(function (file) { // Filter test modules.
+					return /\.test\.js$/.test(file);
+				}).map(function (file) { // Normalize paths to RequireJS module names.
+					return file.replace(/^\/base\/(.*?)\.js$/, '$1');
+				});
 		}
-	};
-	require.config(config);
-	console.log("RequireJS configuration: "+ JSON.stringify(config, null, '  '));
+		require.config(config);
+		console.log("RequireJS configuration: "+ JSON.stringify(config, null, '  '));
+	});
 })();
